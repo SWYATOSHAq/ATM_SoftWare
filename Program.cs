6a62903f5303905b3a32de9b321dad0d6f08b170
User <user@Mac.Dlink>
2025-09-10 21:30:28 +0300

    feet:final version progect

--- Program.cs
+++ Program.cs
@@ -1,12 +1,60 @@
-﻿namespace ATM_SoftWare
+﻿using System.Net.Mail;
+
+namespace ATM_SoftWare
 {
     class Program
     {
+        public static int correct_amount = 0;
         
         
-        static public void Main(string[] args)
+        static int SelectAmount()
         {
+            int amount = int.Parse(Console.ReadLine());
+            return amount;
+        }
+
+        static void AtmWork(int amount)
+        {
+            if (amount % 100 == 0 && amount <= 150000)
+            {
+                int[] denominations = { 5000, 2000, 1000, 500, 200, 100 };//массив с номиналоми купюр
+                Dictionary<int, int> result = new Dictionary<int, int>();//Dictionary (словарь) - это коллекция, которая хранит пары "ключ-значение".P.S посмотрел в инете
+                
+                correct_amount = amount;
+                foreach (int denomination in denominations)
+                {
+                    if (correct_amount >= denomination)
+                    {
+                        int count = correct_amount / denomination;
+                        result[denomination] = count;
+                        correct_amount %= denomination;
+                    }
+                }
+                Console.WriteLine($"для снятия {amount} ден.единиц потребуется:");
+                bool hasBanknotes = false;
+                foreach (int denomination in denominations)
+                {
+                    if (result.ContainsKey(denomination) && result[denomination] > 0)
+                    {
+                        Console.WriteLine($"{result[denomination]} по {denomination} рублей");
+                        hasBanknotes = true;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите корректное значение по указанным ниже критериям:\n 1)сумма для снятия должна быть не более 150 000 ден.единиц\n 2)сумма должна быть кратна 100");
+            }
             
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("МОЙ БАНКОМАТ");
+            Console.WriteLine("Доступные номиналы: 100, 200, 500, 1000, 2000, 5000");
+            Console.WriteLine("Пожалуйста введите сумму, которую хотите снять:\nВнимание,она должна быть кратна 100 и не больше 150 000!");
+            int number = SelectAmount();
+            AtmWork(number);
         }
     }
 }
--- obj/Debug/net9.0/ATM_SoftWare.AssemblyInfo.cs
+++ obj/Debug/net9.0/ATM_SoftWare.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM_SoftWare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+434233ab61241c7fd1c5f281f5fcddde32448090")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM_SoftWare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM_SoftWare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
